ec1d5df83d1e991548f266f9e0c2447ae2d568df
sodri126 <mshodri24@gmail.com>
2018-07-28 16:28:39 +0700

    Revert "Add backend + frontend"
    This reverts commit c7bd17f58221dd9fa6c805530b1eb07cbfaf8fa7.

--- d45hboard/response/choose_beverage.php
+++
@@ -1,8 +0,0 @@
-<?php
-include "../../php/connection/connection.php";
-$id = $_GET['id'];
-$stmt = $conn->prepare("SELECT `bev_id`, `bev_name`, `quantity`, `price` FROM `beverage` WHERE exp_date > NOW() AND `bev_id` = ?");
-$stmt->bind_param("s", $id);
-$stmt->execute();
-$data = $stmt->get_result()->fetch_assoc();
-echo json_encode($data);
--- d45hboard/response/choose_book.php
+++
@@ -1,8 +0,0 @@
-<?php
-include "../../php/connection/connection.php";
-$id = $_GET['id'];
-$stmt = $conn->prepare("SELECT `book_id`, `b_name`, `stock`, `price` FROM `book` WHERE stock > 1 and `book_id` = ?");
-$stmt->bind_param("s", $id);
-$stmt->execute();
-$data = $stmt->get_result()->fetch_assoc();
-echo json_encode($data);
